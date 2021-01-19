f73ec5e2518dbfddfa5109c4f69258b733968724
BernardDev <bernard.devworks@gmail.com>
2021-01-20 00:38:11 +0100

    Heroku deploy #9

--- server/index.js
+++ server/index.js
@@ -12,23 +12,35 @@ app.get('/synths', async (req, res) => {
   res.json(synths);
 });
 
-app.get('/synths', async (req, res) => {
-  const synths = await Synth.findAll();
-  res.json(synths);
-});
-
-// app.get('/synth/:synthId', async (req, res) => {
-//   const synthId = parseInt(req.params.synthId);
-//   const synth = await Synth.findByPk(synthId);
-//   res.json(synth);
-// });
-
 app.get('/synth/:synthId', async (req, res) => {
-  const synthId = parseInt(req.params.synthId);
-  const synth = await Synth.findByPk(synthId, {include: [Specification]});
-  res.json(synth);
+  try {
+    const synthId = parseInt(req.params.synthId);
+    const synth = await Synth.findByPk(synthId, {include: [Specification]});
+    if (synthId) {
+      res.json(synth);
+    } else {
+      res.status(404).send('Synth not found');
+    }
+  } catch (e) {
+    next(e);
+  }
 });
 
+// MANUFACTURERS
+app.get('/manufacturers', async (req, res) => {
+  const manufacturers = await Manufacturer.findAll();
+  res.json(manufacturers);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// -----------------------------
+// 400 client error
+// 404 not found
+
+// EXAMPLES
 // exports.findAll = (req, res) => {
 //   const title = req.query.title;
 //   var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
@@ -45,12 +57,18 @@ app.get('/synth/:synthId', async (req, res) => {
 //     });
 // };
 
-// MANUFACTURERS
-app.get('/manufacturers', async (req, res) => {
-  const manufacturers = await Manufacturer.findAll();
-  res.json(manufacturers);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.get("/users/:userId/lists", async (req, res, next) => {
+//   try {
+//     const userId = parseInt(req.params.userId);
+//     const user = await User.findByPk(userId, {
+//       include: [TodoList],
+//     });
+//     if (user) {
+//       res.send(user.todoLists);
+//     } else {
+//       res.status(404).send("User not found");
+//     }
+//   } catch (e) {
+//     next(e);
+//   }
+// });

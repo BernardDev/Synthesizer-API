f8279481447fe22eb337bbb2f98b6461f16a393f
BernardDev <bernard.devworks@gmail.com>
2021-01-20 00:23:52 +0100

    Heroku deploy #8

--- server/index.js
+++ server/index.js
@@ -12,11 +12,38 @@ app.get('/synths', async (req, res) => {
   res.json(synths);
 });
 
-app.get('/synths/:synthId', async (req, res) => {
+app.get('/synths', async (req, res) => {
+  const synths = await Synth.findAll();
+  res.json(synths);
+});
+
+// app.get('/synth/:synthId', async (req, res) => {
+//   const synthId = parseInt(req.params.synthId);
+//   const synth = await Synth.findByPk(synthId);
+//   res.json(synth);
+// });
+
+app.get('/synth/:synthId', async (req, res) => {
   const synthId = parseInt(req.params.synthId);
-  const synth = await Synth.findByPk(synthId);
+  const synth = await Synth.findByPk(synthId, {include: [Specification]});
   res.json(synth);
 });
+
+// exports.findAll = (req, res) => {
+//   const title = req.query.title;
+//   var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+
+//   Tutorial.findAll({ where: condition })
+//     .then(data => {
+//       res.send(data);
+//     })
+//     .catch(err => {
+//       res.status(500).send({
+//         message:
+//           err.message || "Some error occurred while retrieving tutorials."
+//       });
+//     });
+// };
 
 // MANUFACTURERS
 app.get('/manufacturers', async (req, res) => {
